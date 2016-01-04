bc85565f23ceabf21df9bcc0fe31722d9b93b8ec
Dmitry Binyuk <dmitry.binyuk@chisw.com>
2016-01-04 20:33:49 +0200

    admin config

---
+++ src/App/ProjectBundle/Admin/ProductCategoryAdmin.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\ProjectBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class ProductCategoryAdmin extends Admin
+{
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name', null, array('required' => true, 'label' => 'Имя'))
+            ->add('description', null, array('required' => false, 'label' => 'Описание'))
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('name', null, array('required' => true, 'label' => 'Имя'))
+            ->add('description', null, array('required' => false, 'label' => 'Описание'))
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('name', null, array('required' => true, 'label' => 'Имя'))
+            ->add('description', null, array('required' => false, 'label' => 'Описание'))
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('name')
+            ->add('description', null, array('required' => false, 'label' => 'Описание'))
+        ;
+    }
+}
---
+++ src/App/ProjectBundle/Controller/ProductCategoryAdminController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\ProjectBundle\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController;
+
+class ProductCategoryAdminController extends CRUDController
+{
+
+}
